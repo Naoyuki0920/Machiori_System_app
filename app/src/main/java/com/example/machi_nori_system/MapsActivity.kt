9fa744570d92ffee1f7b17b4e902907f7a73f701
Naoyuki0920 <naoyuki-effort0920@docomo.ne.jp>
2021-10-11 17:01:34 +0900

    [finish] アイコン設置,レスポンシブ対応

--- app/src/main/java/com/example/machi_nori_system/MapsActivity.kt
+++ app/src/main/java/com/example/machi_nori_system/MapsActivity.kt
@@ -7,15 +7,22 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
 import com.example.machi_nori_system.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var latlng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +47,83 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        latlng = LatLng(36.532264, 136.62770)
+        addMarker()
 
         // Add a marker in Sydney and move the camera
         val kanazawa = LatLng(36.56330, 136.65414)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(kanazawa))
+    }
+
+    private fun setIcon(latitude: Double, longitude: Double) {
+        latlng = LatLng(latitude, longitude)
+        val descriptor = BitmapDescriptorFactory.fromResource(R.drawable.machinori_port)
+
+        // 貼り付設定
+        val overlayOptions = GroundOverlayOptions()
+        overlayOptions.image(descriptor)
+        // 画像固定位置
+        overlayOptions.anchor(0.5f, 0.9f)
+
+        // 張り付け画像の大きさ メートル単位
+        // public GroundOverlayOptions	position(LatLng location, float width, float height)
+        overlayOptions.position(latlng, 80f, 160f)
+        overlayOptions.zIndex(10f)
+
+        // マップに貼り付け・アルファを設定
+        val overlay = mMap.addGroundOverlay(overlayOptions)
+        overlay.transparency = 0.0f
+
+    }
+
+
+
+    private fun addMarker() {
+        try {
+            val data = parseJson("Machinori.json")
+            val jsonObj = data.getJSONArray("Machinori")
+            for (i in 0 until jsonObj.length()) {
+                val central = jsonObj.getJSONObject(i)
+                //                Log.d("Check", String.valueOf(central));
+                val port = Port(central)
+                //                setMarker(central_inter.location ,central_inter.lat, central_inter.lng);
+                setIcon(port.lat, port.lng)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    class Port(json: JSONObject) {
+        var id: Int
+        var location: String
+        var lat: Double
+        var lng: Double
+
+        init {
+            id = json.getInt("id")
+            location = json.getString("location")
+            lat = json.getDouble("lat")
+            lng = json.getDouble("lng")
+        }
+    }
+
+    @Throws(JSONException::class, IOException::class)
+    private fun parseJson(file: String): JSONObject {
+        val inputStream = this.assets.open(file)
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        var json: JSONObject? = null
+        var data: String? = ""
+        var str = bufferedReader.readLine()
+        while (str != null) {
+            data += str
+            str = bufferedReader.readLine()
+        }
+        json = JSONObject(data)
+        inputStream.close()
+        bufferedReader.close()
+        return json
     }
 }
